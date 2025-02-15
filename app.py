dcebb5cee8b7f4d12de440a2186df80b4ddc63d6
株式会社VividCreation <vividcreation.info@gmail.com>
2025-02-16 02:42:20 +0900

    Added app.py

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# 環境変数からLINEのアクセストークンとシークレットを取得
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
+
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/")
+def home():
+    return "LINE Bot is running on Heroku!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_text = event.message.text
+
+    # 簡単なレスポンス
+    if "おすすめ" in user_text:
+        reply_text = "おすすめの喫煙可能な飲食店はこちら！"
+    else:
+        reply_text = f"『{user_text}』に関する情報は準備中です。"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_text)
+    )
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
